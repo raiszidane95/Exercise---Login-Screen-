4f0d3f4aa9ca9480cea6deabdf8d4da064e622c3
raiszidane95 <raiszidane95@github.com>
2023-05-31 08:13:06 +0700

    Remove CTA button

--- lib/screen/detail_product/detail_product_screen.dart
+++ lib/screen/detail_product/detail_product_screen.dart
@@ -124,13 +124,6 @@ class _DetailProductScreenState extends State<DetailProductScreen> {
                         child: Text(
                             'All our foods are double checked before leaving our stores so by any case you found a broken food please contact our hotline immediately.'),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Ctabutton(
-                          textTitle: 'Add to cart',
-                          navigationDestination: Cart_Screen(),
-                        ),
-                      )
                     ],
                   ),
                 )
